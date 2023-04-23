61c69898969d4865c253a85c7152701fd581a4f7
VICTORIOUS <70435342+edivalue@users.noreply.github.com>
2023-04-23 07:41:56 +0100

    port scanner script
    This commit adds the initial version of the port scanner script. The script scans a given host for open ports and reports any weak ports found. The script uses threading for faster scanning and saves the results to a text file

---
+++ scanner.py
@@ -0,0 +1,60 @@
+import socket, sys, threading
+
+if len(sys.argv) > 1:
+	target=socket.gethostbyname(sys.argv[1])
+else:
+	target= socket.gethostbyname(input('Enter host\'s name: '))
+
+woo=[]
+
+#type = string
+print('\nHost\'s IP_Address: ',target,'\n')
+print('Searching for weak ports\nLOADING....',end=' ')	
+icounter=0
+
+
+def scanport(target, port):
+	s=socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+	
+	if s == 0:
+		print(' ',target,'~Port %d [open]'%port)
+		print('before close')
+		s.close()
+		icounter=2
+		woo.append('if')
+		
+	else:
+		icounter=1
+		woo.append('else')
+		
+	
+		
+f=[]		
+for i in range(1, 8001):
+	scan=threading.Thread(target=scanport(target, i))
+	scan.start()
+	f.append(scan)	
+	if type(f)==str:
+		with open('scan.txt', 'a') as sf:
+			sf.write(f)
+			sf.close()
+			
+	else:
+		ri=f.pop()
+		r=str(ri)
+		with open('scan1.txt', 'a') as sf1:
+			sf1.writelines(r)
+			
+sf1.close()
+	
+		
+print('End ')
+
+if 'if' in woo:
+	roo=woo.count('if')
+	print(f'{roo} WEAK PORT(S) FOUND')
+else:
+	print('NO WEAK PORTS')
+print()
+#with open('scaner.txt','w') as sy:
+#	sy.write(f)
