23c72ba7d7dea51c1ac7420b4812128ff92d5e47
HACKBOXX <hackboxx@localhost.localdomain>
2023-04-23 21:01:23 +0100

    UPDATED

--- scanner.py
+++ scanner.py
@@ -35,7 +35,7 @@ for i in range(1, 8001):
     scan=threading.Thread(target=scanport, args=(target, i))
     scan.start()
     f.append(scan)
-    if isinstance(ri, str):
+    if isinstance(f, str):
         with open('scan.txt', 'a') as sf:
             sf.write(ri)
     else:
@@ -43,8 +43,7 @@ for i in range(1, 8001):
         R=str(ri)
         with open('scan1.txt', 'a') as sf1:
             sf1.writelines(R)
-			
-    		sf1.close()
+sf1.close()
 	
 		
 print('End ')
@@ -55,5 +54,4 @@ if 'if' in woo:
 else:
 	print('NO WEAK PORTS')
 print()
-#with open('scaner.txt','w') as sy:
-#	sy.write(f)
+
